0f0d1b45b1e37ec6cd86dc558ba555b7e3ce7ba7
Tommy <sasida@hotmail.fr>
2023-04-20 12:55:43 +0200

    Ajout député

---
+++ Cher député/lib/depute.rb
@@ -0,0 +1,19 @@
+  require 'nokogiri'
+  require 'open-uri'
+
+  begin
+    page = Nokogiri::HTML(URI.open("https://www.voxpublic.org/spip.php?page=annuaire&cat=deputes&pagnum=900"))
+  rescue StandardError => erreur
+    puts "Erreur lors de l'ouverture de l'URL : #{erreur}"
+  end
+
+  emails = page.xpath('//a[contains(@href, "mailto") and contains (@href, "assemblee")]/text()').map(&:text).map(&:strip)
+
+  nommails = []
+
+  emails.each do |email|
+    prenom, nom = email.split("@")[0].split(".").map(&:capitalize)
+    nommails << [prenom, nom, email]
+  end
+
+  puts nommails
