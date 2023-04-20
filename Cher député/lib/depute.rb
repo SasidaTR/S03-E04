647aaa4b7e0b24ae75206cf8d59343279404058b
Tommy <sasida@hotmail.fr>
2023-04-20 13:14:16 +0200

    Correction depute

--- Cher député/lib/depute.rb
+++ Cher député/lib/depute.rb
@@ -1,19 +1,23 @@
-  require 'nokogiri'
-  require 'open-uri'
+require 'nokogiri'
+require 'open-uri'
 
-  begin
-    page = Nokogiri::HTML(URI.open("https://www.voxpublic.org/spip.php?page=annuaire&cat=deputes&pagnum=900"))
-  rescue StandardError => erreur
-    puts "Erreur lors de l'ouverture de l'URL : #{erreur}"
-  end
+begin
+  page = Nokogiri::HTML(URI.open("https://www.voxpublic.org/spip.php?page=annuaire&cat=deputes&pagnum=900"))
+rescue StandardError => erreur
+  puts "Erreur lors de l'ouverture de l'URL : #{erreur}"
+end
 
-  emails = page.xpath('//a[contains(@href, "mailto") and contains (@href, "assemblee")]/text()').map(&:text).map(&:strip)
+emails = page.xpath('//a[contains(@href, "mailto") and contains (@href, "assemblee")]/text()').map(&:text).map(&:strip)
 
-  nommails = []
+nommails = []
 
-  emails.each do |email|
-    prenom, nom = email.split("@")[0].split(".").map(&:capitalize)
-    nommails << [prenom, nom, email]
-  end
+emails.each do |email|
+  prenom, nom = email.split("@")[0].split(".").map(&:capitalize)
+  nommails << {
+    "first_name" => prenom,
+    "last_name" => nom,
+    "email" => email
+  }
+end
 
-  puts nommails
+puts nommails
